072741fa062d1a0ce34349f28b32d8b249bbaa40
carlos-hp <carloshp@outlook.com>
2021-10-31 10:43:33 -0300

    test: add unit test(note)

--- app/src/main/java/co/tiagoaguiar/evernotekt/model/Note.kt
+++ app/src/main/java/co/tiagoaguiar/evernotekt/model/Note.kt
@@ -1,5 +1,9 @@
 package co.tiagoaguiar.evernotekt.model
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  *
  * Setembro, 24 2019
@@ -11,4 +15,21 @@ data class Note(
     var desc: String? = null,
     var date: String? = null,
     var body: String? = null
-)
+) {
+    val createdDate: String
+        get() {
+            if (this.date.isNullOrBlank())
+                return ""
+
+            return try {
+                val locale = Locale("pt", "Br")
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", locale)
+                val resultDate = SimpleDateFormat("MMM yyyy", locale).format(dateFormat.parse(date!!)!!)
+                resultDate.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+                }
+            } catch (e: ParseException) {
+                ""
+            }
+        }
+}
---
+++ app/src/test/java/co/tiagoaguiar/evernotekt/NoteTest.kt
@@ -0,0 +1,29 @@
+package co.tiagoaguiar.evernotekt
+
+import co.tiagoaguiar.evernotekt.model.Note
+import org.junit.Assert
+import org.junit.Test
+
+class NoteTest {
+
+    @Test
+    fun `test should format date pattern to month and year`() {
+        val note = Note(title = "Nota A", body = "Nota A Conteudo", date = "20/02/2019")
+
+        Assert.assertEquals("Fev 2019", note.createdDate)
+    }
+
+    @Test
+    fun `test should format date cases empty`() {
+        val note = Note(title = "Nota A", body = "Nota A Conteudo", date = "")
+
+        Assert.assertEquals("", note.createdDate)
+    }
+
+    @Test
+    fun `test should format date case null`() {
+        val note = Note(title = "Nota A", body = "Nota A Conteudo")
+
+        Assert.assertEquals("", note.createdDate)
+    }
+}
