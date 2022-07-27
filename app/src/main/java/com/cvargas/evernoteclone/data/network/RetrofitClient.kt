16cddf79fbbdb57a5582237e9e2ae8d840fb5344
carlos-hp <carloshp@outlook.com>
2022-07-26 21:56:44 -0300

    chore: update build.gradle

--- app/src/main/java/com/cvargas/evernoteclone/data/network/RetrofitClient.kt
+++ app/src/main/java/com/cvargas/evernoteclone/data/network/RetrofitClient.kt
@@ -9,11 +9,12 @@ object RetrofitClient {
 
     private const val EVERNOTE_BASE_URL = "https://myevernote.glitch.me/"
 
-    val evernoteApi: EvernoteAPI = Retrofit.Builder()
-        .baseUrl(EVERNOTE_BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .build()
-        .create(EvernoteAPI::class.java)
-
+    val evernoteApi: EvernoteAPI by lazy {
+        Retrofit.Builder()
+            .baseUrl(EVERNOTE_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(EvernoteAPI::class.java)
+    }
 }
